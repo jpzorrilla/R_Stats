3dd3913035ac625b0881b45d16f84a3197487703
Juan Pablo Zorrilla <60937748+jpzorrilla@users.noreply.github.com>
2020-09-22 18:19:24 -0300

    Delete Calculo MSE.R

--- Calculo MSE.R
+++
@@ -1,5 +0,0 @@
-y_obs<-c(-3.9, 8.3, -1.8, -1.7, -6.0, 0.9, 0.7, -0.5, 1.6, 3.7, -1.1,-3.2) #datos observados
-y_pred<-c(-1.1, 2.0, 1.4, -0.8, 6.8, 3.4, 4.0, 0.8, 2.9, 10.0, 6.0, 0.8) #datos predichos
-install.packages("Metrics")
-library(Metrics)
-mse(y_obs, y_pred) #Mean Squared Error regression loss
